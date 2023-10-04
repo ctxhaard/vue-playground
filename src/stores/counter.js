19a74ad0330a058090e1684d49d5baa6f5aff7d2
Carlo Tomasin <carlo.tomasin@icare-world.com>
2023-10-04 10:23:42 +0200

    first moves on Pinia

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,12 +1,22 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useCounterStore = defineStore('counter', {
+  state: () => ({
+    user: {
+      name: 'Mario',
+      surname: 'Rossi',
+      contacts: {
+        phone: '+39 123 4567',
+        email: 'mario.rossi@google.com'
+      }
+    },
+    pings: 0,
+    status: null,
+    sessionSince: null
+  }),
+  actions: {
+    increment() {
+      this.pings++
+    }
   }
-
-  return { count, doubleCount, increment }
 })
---
+++ src/views/PlaygroundView.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+import { useCounterStore } from '../stores/counter'
+
+const store = useCounterStore()
+
+const emails = [
+  'mario.rossi@yahoo.it',
+  'andrea.verdi@libero.it',
+  'luca.bianchi@tin.it',
+  'francesco.marrone@google.com'
+]
+
+function onButtonClick() {
+  store.increment();
+  store.user.contacts.email = emails[Math.round(Math.random() * (emails.length -1 ))]
+  console.log(`Button pressed! store.pings: ${ store.pings }`);
+  store.$patch({
+    user: {
+      surname: 'Bianchi'
+    }
+  })
+}
+
+</script>
+
+<template>
+  <div>
+    <p>This is my playground</p>
+    <p><button @click="onButtonClick">Click me</button></p>
+    <p><input v-model="store.user.contacts.email" type="text" placeholder="Put a text here"/></p>
+    <p><input v-model="store.pings" type="number" placeholder="Put a number here"/></p>
+    <p>name: {{ store.user.name || '?' }}, surname: {{ store.user.surname || '?' }},
+      email: {{ store.user.contacts.email || '?' }}, phone: {{ store.user.contacts.phone || '?' }}</p>
+  </div>
+</template>
